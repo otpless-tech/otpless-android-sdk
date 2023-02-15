8f1e0d2af6dabe52499d351b6833100e47d3ddea
Digvijay Singh <digvijay@otpless.com>
2023-02-15 13:16:22 +0530

    * launch method made public to get called from custom buttons. * config method is created to change color and text on otpless screen

--- otpless-android-sdk/src/main/java/com/otpless/views/OtplessManager.java
+++ otpless-android-sdk/src/main/java/com/otpless/views/OtplessManager.java
@@ -1,7 +1,11 @@
 package com.otpless.views;
 
-import android.content.Context;
+import static com.otpless.utils.Utility.isNotEmpty;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 public class OtplessManager {
@@ -30,7 +34,49 @@ public class OtplessManager {
         this.mOtpImpl.add(activity);
     }
 
-    void launch(final Context context, final String link, final OtplessUserDetailCallback callback) {
+    public void launch(final Context context, final String link, final OtplessUserDetailCallback callback) {
         this.mOtpImpl.launch(context, link, callback);
+    }
+
+    public void setConfiguration(@NonNull final Context context, String backgroundColor,
+                                 String loaderColor, String messageText, String messageColor,
+                                 String cancelButtonText, String cancelButtonColor
+    ) {
+        final SharedPreferences pref = context.getSharedPreferences("otpless_configuration", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = pref.edit();
+        if (isNotEmpty(backgroundColor)) {
+            editor.putString("screen_bg_color", backgroundColor);
+        }
+        if (isNotEmpty(loaderColor)) {
+            editor.putString("loader_color", loaderColor);
+        }
+        if (isNotEmpty(messageText)) {
+            editor.putString("message_text", messageText);
+        }
+        if (isNotEmpty(messageColor)) {
+            editor.putString("message_color", messageColor);
+        }
+        if (isNotEmpty(cancelButtonText)) {
+            editor.putString("cancel_btn_text", cancelButtonText);
+        }
+        if (isNotEmpty(cancelButtonColor)) {
+            editor.putString("cancel_btn_color", cancelButtonColor);
+        }
+        editor.apply();
+    }
+
+    /**
+     * return string array of length 6
+     */
+    public String[] getConfiguration(final Context context) {
+        final String[] result = new String[6];
+        final SharedPreferences pref = context.getSharedPreferences("otpless_configuration", Context.MODE_PRIVATE);
+        result[0] = pref.getString("screen_bg_color", null);
+        result[1] = pref.getString("loader_color", null);
+        result[2] = pref.getString("message_text", null);
+        result[3] = pref.getString("message_color", null);
+        result[4] = pref.getString("cancel_btn_text", null);
+        result[5] = pref.getString("cancel_btn_color", null);
+        return result;
     }
 }
